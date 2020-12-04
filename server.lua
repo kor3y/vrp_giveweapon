2507875f71037f2f9e58c668d7cc73726ada97cf
korey <75332399+kor3y@users.noreply.github.com>
2020-12-04 09:01:24 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -5,8 +5,8 @@ vRP = Proxy.getInterface("vRP")
 vRPclient = Tunnel.getInterface("vRP","vrp_give_weapons")
 
 RegisterCommand("giveweapon", function(player, args)
-    local user_id = vRP.getUserId(player)
-    if vRP.hasPermission(user_id, 'admin.giveweapons') then
+    local user_id = vRP.getUserId({player})
+    if vRP.hasPermission({user_id, 'admin.giveweapons'}) then
         TriggerClientEvent('give:weapon', player, args[1], args[2])
     end
 end)
